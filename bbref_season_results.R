2e36b2ec934693338aeaea6f823376b5790e86ce
Michael Fichman <mfichman@gmail.com>
2024-05-06 10:38:00 -0400

    Update bbref_season_results.R

--- bbref_season_results.R
+++ bbref_season_results.R
@@ -1,25 +1,35 @@
 library(tidyverse)
 library(baseballr)
+library(lubridate)
 
 # Here's a fun challenge - can we grab data from the Baseball Reference API and look for spurious correlations?
 
-phi <- bref_team_results("PHI", 2022) %>%
+phi <- bref_team_results("PHI", 2021) %>%
+  rbind(., bref_team_results("PHI", 2022)) %>%
+  rbind(., bref_team_results("PHI", 2023)) %>%
+  rbind(., bref_team_results("PHI", 2024)) %>%
   mutate(win = ifelse(str_detect(Result,"W") == TRUE, 1, 0),
          loss = ifelse(str_detect(Result, "L") == TRUE, 1, 0),
          wins = cumsum(win),
          losses = cumsum(loss),
          win_pct = 100*(wins / (wins + losses)),
-         delmelle = ifelse(Gm > 77, "2. After Elizabeth", "1. Before Elizabeth"))
+         delmelle = ifelse(Gm < 78 & Year == 2022 |
+                             Year == 2021, "1. Before Elizabeth", "2. After Elizabeth"))
   
-nym <- bref_team_results("NYM", 2022)  %>%
+nym <-  bref_team_results("NYM", 2021) %>%
+  rbind(., bref_team_results("NYM", 2022))  %>%
+  rbind(., bref_team_results("NYM", 2023)) %>%
+  rbind(., bref_team_results("NYM", 2024)) %>%
   mutate(win = ifelse(str_detect(Result,"W") == TRUE, 1, 0),
          loss = ifelse(str_detect(Result, "L") == TRUE, 1, 0),
          wins = cumsum(win),
          losses = cumsum(loss),
          win_pct = 100*(wins / (wins + losses)),
-         delmelle = ifelse(Gm > 77, "2. After Elizabeth", "1. Before Elizabeth"))
+         delmelle = ifelse(Gm < 78 & Year == 2022 |
+                             Year == 2021, "1. Before Elizabeth", "2. After Elizabeth"))
 
-# number of wins as of 11/1 - added 11-3 record for Phillies in playoffs, 1-2 for Mets
+# number of wins as of 4/13/2023 - added 11-6 record for Phillies in playoffs, 1-2 for Mets
+# Phillies 4-8 in regular season, Mets 7-6
 
 nym %>% 
   rbind(., phi) %>%
@@ -27,15 +37,34 @@ nym %>%
   group_by(delmelle, Tm) %>%
   summarize(wins = sum(win),
             losses = sum(loss)) %>%
-  mutate(wins = ifelse(Tm == "PHI", wins +11, wins +1),
-         losses = ifelse(Tm == "PHI", losses + 3, losses +2),
-         win_pct = 100*(wins / (wins + losses))) %>%
+  mutate(win_pct = 100*(wins / (wins + losses))) %>%
+  ungroup() %>%
   ggplot()+
-  geom_bar(aes(y = win_pct, x = Tm, fill = delmelle), 
-           stat = "identity", position = "dodge")+
-  scale_fill_manual(values = c("blue", "red"))+
-  labs(title = "Winning Percentages (Incl Playoffs) By MUSA Administration - 2022",
-          subtitle = "Phillies Performance Positively Associated With Delmelle Admin\nMets Unaffected",
-          caption = "Data - Baseball Reference")+
+  geom_point(aes(y = win_pct, x = delmelle, color = Tm, group = Tm))+
+  geom_line(aes(y = win_pct, x = delmelle, color = Tm, group = Tm),
+            alpha = 0.6)+
+  scale_color_manual(values = c("blue", "red"))+
+  ylim(45, 65)+
+  labs(title = "Delmelle Locational Effect on Major League Winning Percentages\n2021 - Present (Regular Season)",
+          subtitle = "Strong, Positive, Causal Effect Leads to Phillies Success\nMets Falling Into Statistically Significant Despair",
+       y = "Winning Percentage",   
+       caption = "Data - Baseball Reference, 5/6/24")+
   theme_minimal()
 
+
+nym %>% 
+  rbind(., phi) %>%
+  mutate(date = mdy(paste(Date, Year))) %>%
+  ggplot()+
+  geom_line(aes(x = date, y = win_pct, color = Tm))+
+  theme_minimal()
+
+
+nym %>%
+  mutate(game = row_number()) %>%
+  rbind(., phi %>%
+          mutate(game = row_number())) %>%
+  ggplot()+
+  geom_line(aes(x = game, y = win_pct, color = Tm))+
+  scale_color_manual(values = c("blue", "red"))+
+  theme_minimal()
